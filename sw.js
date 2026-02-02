12068ac6b8d247731abe90e041616d28117b4b45
stella47556528 <75627638+stella47556528@users.noreply.github.com>
2026-02-02 15:36:18 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+// 簡易版 Service Worker 用於離線快取
+const CACHE_NAME = 'planner-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
